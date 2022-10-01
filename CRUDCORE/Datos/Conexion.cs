e6c0547099fd05810f6308c7470c9355ed5cba98
Juan Melichio <juanmelichio@gmail.com>
2022-10-01 14:38:15 -0300

    Primer commit proyecto CRUD CORE

---
+++ CRUDCORE/Datos/Conexion.cs
@@ -0,0 +1,29 @@
+﻿//Añado la referencia SQLClient que es el paquete que se instala en .net core para poder generar la conexión con la bd
+using System.Data.SqlClient;
+
+
+namespace CRUDCORE.Datos
+{
+    public class Conexion
+    {
+        
+        private string cadenaSQL=string.Empty;
+
+        //Creo el constructor con el mismo nombre que la clase Conexion. El constructor es lo primero que se ejecuta
+        //una vez que se le hace una instancia a la clase de Conexión.
+        public Conexion()
+        {
+            //obtengo en builder la cadena de conexión que está en mi archivo appsetings.json 
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+
+            //obtengo en mi variable privada cadenasql una sección particular dentro de mi archivo appsettings.json que es el string de conexion 
+            cadenaSQL = builder.GetSection("ConnectionStrings:CadenaSQL").Value;
+        }
+
+        //Creo un metodo que me devuelve la cadena SQL que obtuvimos
+        public string getCadenaSQL()
+        {
+            return cadenaSQL;
+        }
+    }
+}
---
+++ CRUDCORE/Datos/ContactoDatos.cs
@@ -0,0 +1,17 @@
+﻿//Esta clase es para acceder a los métodos que tengo en mi bd CRUD
+
+
+//Primera referencia es para mi carpeta de Modelos
+using CRUDCORE.Models;
+//Referencia que me permite utilizar los objetos y clases de SQL
+using System.Data.SqlClient;
+
+//También referencia a systema Data
+using System.Data;
+
+namespace CRUDCORE.Datos
+{
+    public class ContactoDatos
+    {
+    }
+}
